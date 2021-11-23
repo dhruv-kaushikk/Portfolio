cdd7ff63efe57e636c4c3a721bebe782c3d7508f
AuroraTook <dhruvkaushik170@gmail.com>
2021-11-23 22:48:01 +0530

    Updates

---
+++ precache-manifest.4e54fc028f7a32ccaf46900d99d3915c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "686705c80889f28b09e60241d9f65eaa",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "359c2b79f8fb9ed49df2",
+    "url": "/Portfolio/static/css/2.32cb7bf3.chunk.css"
+  },
+  {
+    "revision": "9179b26e4550f29dee29",
+    "url": "/Portfolio/static/css/main.8b9d5a47.chunk.css"
+  },
+  {
+    "revision": "359c2b79f8fb9ed49df2",
+    "url": "/Portfolio/static/js/2.d07dea78.chunk.js"
+  },
+  {
+    "revision": "9179b26e4550f29dee29",
+    "url": "/Portfolio/static/js/main.20606ad3.chunk.js"
+  },
+  {
+    "revision": "781b852bfd02ab596b93",
+    "url": "/Portfolio/static/js/runtime-main.cde3a7e1.js"
+  },
+  {
+    "revision": "54c8b12a4400ad41b9c408c170cc2070",
+    "url": "/Portfolio/static/media/cv1.54c8b12a.png"
+  },
+  {
+    "revision": "4c1cba5f7496cf94240aa8aa3d67f76a",
+    "url": "/Portfolio/static/media/cv2.4c1cba5f.png"
+  },
+  {
+    "revision": "2da239b51f31c877b33bb3bf4e81c849",
+    "url": "/Portfolio/static/media/cv3.2da239b5.png"
+  },
+  {
+    "revision": "fdf35f0af93d30d092cb284563dbb732",
+    "url": "/Portfolio/static/media/cv4.fdf35f0a.png"
+  },
+  {
+    "revision": "0a04cd22a69442e31c3df8f8e1adff1b",
+    "url": "/Portfolio/static/media/foad_logo.0a04cd22.jpg"
+  },
+  {
+    "revision": "c31b5069f157b82943809ab2ecc38ef8",
+    "url": "/Portfolio/static/media/git.c31b5069.svg"
+  }
+]);
